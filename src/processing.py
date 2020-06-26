3cd84685a330f80ecefa4078439fb79b1df14e6a
Nedeljko Vignjevic <vignjevic.nedeljko1999@gmail.com>
2020-06-26 15:07:04 +0200

    Added headers to source code files.

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,19 @@
+"""
+MIT License
+
+chatbot is relatively simple AI-based software application that simulates human conversation through text chats.
+This file is part of chatbot.
+
+Copyright (c) 2020 Nedeljko Vignjević
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+"""
+
 import numpy as np
 import json
 
